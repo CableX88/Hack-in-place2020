4faf36d42d16971fd229cbbc72438a8d6d3a3687
tsangryan <ryan.ying.tsang@gmail.com>
2020-08-16 15:38:55 -0700

    unified html files, no real behaviors done

--- app/routes.py
+++ app/routes.py
@@ -1,40 +1,71 @@
 from flask import render_template
 from app import app
-from flask import request
+from flask import request, redirect, url_for
+
+dummyusers = {"johndoe@ex.com": "password"}
+current_user = 0
 
 @app.route('/', methods=['POST','GET'])
 @app.route('/login', methods=['POST','GET'])
 def login():
-    pass
+	if request.method == 'POST':
+		if request.form['signin'] == "Create an Account":
+			return redirect(url_for('create_account'))
+		email = request.form['email']
+		password = request.form['password']
+		# look up the user
+		if email in dummyusers.keys():
+			if password == dummyusers[email]:
+				global current_user
+				current_user = email
+				return redirect(url_for('index'))
+	return render_template('ConnectWorkLogin.html')
 
 @app.route('/create-account', methods=['POST', 'GET'])
 def create_account():
-	pass
+	if request.method == 'POST':
+		fullname = request.form['fname']
+		email = request.form['email']
+		password = request.form['pwd']
+		global dummyusers, current_user
+		dummyusers[email] = password
+		current_user = email
+		return redirect(url_for('index'))
+	return render_template('create-account.html')
 
 @app.route('/index', methods=['POST', 'GET'])
 def index():
-	pass
+	if request.method == 'POST':
+		if 'profile' in request.form:
+			return redirect(url_for('my_profile'))
+		if 'academic' in request.form:
+			return redirect(url_for('academic'))
+		if 'social' in request.form:
+			return redirect(url_for('social'))
+		if 'wellness' in request.form:
+			return redirect(url_for('wellness'))
+	return render_template('index.html')
 
 @app.route('/my-profile', methods=['POST', 'GET'])
 def my_profile():
-	if 
+	return render_template('my-profile.html')
 
 @app.route('/academic', methods=['POST', 'GET'])
 def academic():
-	pass
+	return render_template('academic.html')
 
 @app.route('/social', methods=['POST', 'GET'])
 def social():
-	pass
+	return render_template('social.html')
 
 @app.route('/wellness', methods=['POST', 'GET'])
 def wellness():
-	pass
+	return render_template('wellness.html')
 
 @app.route('/connection', methods=['GET'])
 def connection():
-	pass
+	return render_template('connection.html')
 
-@app.route('/clubs')
-def clubs():
-	pass
+# @app.route('/clubs')
+# def clubs():
+# 	pass
